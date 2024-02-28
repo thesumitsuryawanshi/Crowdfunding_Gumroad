c77ced4449c5a93d31c7c68e535880abadc2b6a2
thesumitsuryawanshi <thesumit0013@gmail.com>
2024-02-28 18:59:03 +0530

    AmountEarned section clear

--- src/components/amountEarned.js
+++ src/components/amountEarned.js
@@ -4,6 +4,12 @@ import { useScrollTrigger } from "@mui/material";
 
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import coin1 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/coin1.png";
+import coin2 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/coin2.png";
+import coin3 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/coin3.png";
+import coin4 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/coin4.png";
+import coin5 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/gcoins.png";
+import coin6 from "../assets/MyImages/GumroadallSVG/Presentation screen SVG/gcoins.png";
 
 function BuiltByDevelopers() {
   const trigger = useScrollTrigger({
@@ -41,7 +47,8 @@ function BuiltByDevelopers() {
             variant="h1"
             color="black"
             textAlign="center"
-            sx={{ fontFamily: "serif", fontSize: 140 }}
+            fontFamily={"Mabry pro"}
+            sx={{ fontFamily: "Mabry pro, sans-serif ", fontSize: 160 }}
           >
             $3,117,343
           </Typography>
@@ -51,12 +58,90 @@ function BuiltByDevelopers() {
             color="black"
             m={5}
             textAlign="center"
-            sx={{ fontFamily: "serif", fontSize: 27 }}
+            sx={{ fontFamily: "Mabry pro, sans-serif ", fontSize: 27 }}
           >
             The amount of income earned by Gumroad <br></br> digital
             entrepreneurs last week.
           </Typography>
         </Grid>
+      </Box>
+
+      <Box
+        sx={{
+          position: "absolute",
+          left: 1650,
+          top: 8700,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin1} alt="coin1" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 1150,
+          top: 8990,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin3} alt="coin3" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 1250,
+          top: 8490,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin2} alt="coin2" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 10,
+          top: 8640,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin3} alt="coin3" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 350,
+          top: 8500,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin4} alt="coin4" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 1450,
+          top: 8900,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin6} alt="coin4" />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 250,
+          top: 8900,
+          zIndex: 1230,
+          transition: "transform 0.1s ease",
+        }}
+      >
+        <img src={coin5} alt="coin5" />
       </Box>
     </Grid>
   );
