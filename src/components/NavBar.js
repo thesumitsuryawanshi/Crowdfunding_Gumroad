8bf2e3b7e40a3489928c919dc2b176d6a21418b6
thesumitsuryawanshi <thesumit0013@gmail.com>
2024-02-29 17:11:29 +0530

    navbar stickyness implementation is going on.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,84 +13,90 @@ function DefaultNavBar() {
   };
 
   return (
-    <Box border="1px solid black">
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={10}>
-          <Box
-            sx={{
-              display: "flex",
-              gap: "20px",
-              alignItems: "center",
-              flexGrow: 1,
-              p: 1,
-            }}
+    <Grid position="sticky">
+      <Box border="1px solid black" position="sticky">
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={10}>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "20px",
+                alignItems: "center",
+                flexGrow: 1,
+                p: 1,
+              }}
+            >
+              {/* Updated Button components */}
+              <Typography
+                {...linkProps}
+                padding="13px 0px 13px 20px"
+                fontSize="18px"
+                fontFamily="Mabry pro"
+                color="#000"
+              >
+                Features
+              </Typography>
+              <Typography
+                {...linkProps}
+                padding="13px"
+                fontSize="18px"
+                fontFamily="Mabry pro"
+                color="#000"
+              >
+                Pricing
+              </Typography>
+              <Typography
+                {...linkProps}
+                padding="13px"
+                fontSize="18px"
+                fontFamily="Mabry pro"
+                color="#000"
+              >
+                Blog
+              </Typography>
+              <Typography
+                {...linkProps}
+                padding="13px"
+                fontSize="18px"
+                fontFamily="Mabry pro"
+                color="#000"
+              >
+                Discover
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{ display: "flex", justifyContent: "flex-end" }}
           >
-            {/* Updated Button components */}
+            {/* Updated Typography components */}
             <Typography
               {...linkProps}
-              padding="13px 0px 13px 20px"
-              fontSize="18px"
+              padding="18px 35px"
+              fontSize="21px"
               fontFamily="Mabry pro"
-              color="#000"
+              border="1px solid black"
+              marginRight={0} // Remove margin-right
+              color="#000" // Changed color to white
             >
-              Features
+              Login
             </Typography>
             <Typography
               {...linkProps}
-              padding="13px"
-              fontSize="18px"
               fontFamily="Mabry pro"
-              color="#000"
-            >
-              Pricing
-            </Typography>
-            <Typography
-              {...linkProps}
-              padding="13px"
+              padding="18px 30px"
+              border="1px solid black"
+              backgroundColor="#000"
               fontSize="18px"
-              fontFamily="Mabry pro"
-              color="#000"
+              color="#fff" // Changed color to white
             >
-              Blog
+              Start Selling
             </Typography>
-            <Typography
-              {...linkProps}
-              padding="13px"
-              fontSize="18px"
-              fontFamily="Mabry pro"
-              color="#000"
-            >
-              Discover
-            </Typography>
-          </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
-          {/* Updated Typography components */}
-          <Typography
-            {...linkProps}
-            padding="18px 35px"
-            fontSize="21px"
-            fontFamily="Mabry pro"
-            border="1px solid black"
-            marginRight={0} // Remove margin-right
-            color="#000" // Changed color to white
-          >
-            Login
-          </Typography>
-          <Typography
-            {...linkProps}
-            fontFamily="Mabry pro"
-            padding="18px 30px"
-            border="1px solid black"
-            backgroundColor="#000"
-            fontSize="18px"
-            color="#fff" // Changed color to white
-          >
-            Start Selling
-          </Typography>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </Grid>
   );
 }
 
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,13 +1,11 @@
 // @mui material components
-import { Link } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/system";
-
+import AppBar from "@mui/material/AppBar";
 // Material Kit 2 React components
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/joy/Button";
 
 //myip's
 import * as React from "react";
@@ -31,6 +29,7 @@ import TwoText from "./TwoText";
 import TopAnimationGuy from "./TopAnimationGuy";
 import AmountEarned from "./amountEarned";
 import Seventeenblocks from "./seventeenblocks";
+import { Toolbar } from "@mui/material";
 
 //myip's
 
@@ -95,60 +94,64 @@ function Presentation() {
     window.open("http://localhost:3000/crowdfunding");
   };
   return (
-    <div className="App">
-      <img
-        src="https://assets-global.website-files.com/6171b265e5c8aa59b42c3472/618ea7afd990103829d614ff_gumroad-logo.svg"
-        width="100%"
-        height="100%"
-      ></img>
-      <NavBar sty />
-      <Firstdivisionofgumroad></Firstdivisionofgumroad>
-      <TopAnimationGuy />
-      <TwoText></TwoText>
-      <TwoSVGS></TwoSVGS>
-      <MakeYourOwnRoad />
-      <SellAnything />
-      <MaxulichneyGuySection />
-      <SellToAnyone />
-      <TrendSVCGuySection />
-      <SellAnywhere />
-      <Seventeenblocks />
-      <SmileyGirl />
-      <AmountEarned />
-      <PeopleInPark />
-      <StartSelling />
-      <Box p={3} m={3} border="1px solid black" borderRadius="12px">
-        <Container>
-          <Grid item xs={12}>
-            <Typography variant="h1" fontWeight="bold" mb={0.5}>
-              Crowfunding
-            </Typography>
-            <Typography variant="body1" color="text">
-              Enter a new realm with Gumroad, where <br></br> creators and
-              customers alike can step <br></br> into the role of investors,
-              transforming the crowdfunding landscape.
-            </Typography>
-          </Grid>
+    <AppBar position="sticky">
+      <Toolbar>
+        <div className="App">
+          <img
+            src="https://assets-global.website-files.com/6171b265e5c8aa59b42c3472/618ea7afd990103829d614ff_gumroad-logo.svg"
+            width="100%"
+            height="100%"
+          ></img>
+          <NavBar />
+          <Firstdivisionofgumroad></Firstdivisionofgumroad>
+          <TopAnimationGuy />
+          <TwoText></TwoText>
+          <TwoSVGS></TwoSVGS>
+          <MakeYourOwnRoad />
+          <SellAnything />
+          <MaxulichneyGuySection />
+          <SellToAnyone />
+          <TrendSVCGuySection />
+          <SellAnywhere />
+          <Seventeenblocks />
+          <SmileyGirl />
+          <AmountEarned />
+          <PeopleInPark />
+          <StartSelling />
+          <Box p={3} m={3} border="1px solid black" borderRadius="12px">
+            <Container>
+              <Grid item xs={12}>
+                <Typography variant="h1" fontWeight="bold" mb={0.5}>
+                  Crowfunding
+                </Typography>
+                <Typography variant="body1" color="text">
+                  Enter a new realm with Gumroad, where <br></br> creators and
+                  customers alike can step <br></br> into the role of investors,
+                  transforming the crowdfunding landscape.
+                </Typography>
+              </Grid>
 
-          <ButtonContainer sx={{ m: 6 }}>
-            <BoxYellow>
-              <BoxRed>
-                <MyBtnStartSelling
-                  onClick={handleStartSellingClick} // Add onClick event
-                  sx={{ px: 9, py: 1.5 }}
-                  Link
-                >
-                  Crowdfunding
-                </MyBtnStartSelling>{" "}
-              </BoxRed>
-            </BoxYellow>
-          </ButtonContainer>
-        </Container>
-      </Box>
-      <Box pt={2} mt={2}>
-        <Footer />
-      </Box>
-    </div>
+              <ButtonContainer sx={{ m: 6 }}>
+                <BoxYellow>
+                  <BoxRed>
+                    <MyBtnStartSelling
+                      onClick={handleStartSellingClick} // Add onClick event
+                      sx={{ px: 9, py: 1.5 }}
+                      Link
+                    >
+                      Crowdfunding
+                    </MyBtnStartSelling>{" "}
+                  </BoxRed>
+                </BoxYellow>
+              </ButtonContainer>
+            </Container>
+          </Box>
+          <Box pt={2} mt={2}>
+            <Footer />
+          </Box>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
